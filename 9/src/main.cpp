50ffcef5e270076589c81e8a1cf06dc7b40d0d41
Stefano Montesi <dincio.montesi@gmail.com>
2018-07-05 16:12:27 +0200

    9 is still too slow...

--- 9/src/main.cpp
+++ 9/src/main.cpp
@@ -6,21 +6,19 @@
 const unsigned int TARGET_SUM = 1000;
 
 int main() {
-    size_t x = 1, y = 1;
-    for(float a = 1;;++x) {
-        for(float b = a + 1;;++b) {
-            auto c = std::sqrt(std::pow(a, 2)) +  (std::pow(b, 2));
-            auto sum = a + b + c;
-            if(sum > TARGET_SUM) {
-                break;
-            }
-            if(std::floor(c) == c && a + b + c == TARGET_SUM) {
-                std::cout << "found 'em: (" << a << ", " << b << ", " << c << ")" << std::endl;
-                return 0;
+    for(int a = 1;; ++a) {
+        for(int b = a;; ++b) {
+            for(int c = 1;; ++c) {
+                if(std::pow(a, 2) + std::pow(b, 2) == std::pow(c, 2) &&
+                   a + b + c == TARGET_SUM) {
+                    std::cout << "found 'em: " << a << ", " << b << ", " << c << std::endl;
+                    return 0;
+                }
             }
         }
     }
 
     std::cout << "did not find 'em..." << std::endl;
-    return 1;
+    return -1;
 }
+
