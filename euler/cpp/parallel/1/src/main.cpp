fcd5f1306811fad7cd2da83f3449c66e07ae3080
Stefano Montesi <dincio.montesi@gmail.com>
2018-10-13 10:47:08 +0200

    Finished implementation of void_invoke_result and void_invoke.

--- euler/cpp/parallel/1/src/main.cpp
+++ euler/cpp/parallel/1/src/main.cpp
@@ -10,25 +10,38 @@
 #include <chrono>
 #include <optional>
 
+template <class T>
+struct type_of {
+    using type = T;
+};
+
 struct void_type { };
 
-template <class T>
-struct type_of { using type = T; };
+std::ostream& operator<<(std::ostream& ostream, const void_type&) {
+    ostream << "{void_type}";
+    return ostream;
+}
 
-template <class Fun, class... Args>
+template <class Fun, class = void, class... Args>
 struct void_invoke_result : std::invoke_result<Fun, Args...> {};
 
 template <class Fun, class... Args>
-struct void_invoke_result<void> : type_of<void_type> {};
+struct void_invoke_result<Fun,
+                          std::enable_if_t<
+                              std::is_void_v<
+                                  std::invoke_result_t<Fun, Args...>
+                              >
+                          >,
+                          Args...> : type_of<void_type> {};
 
-template <class T>
-using void_invoke_result_t = typename void_invoke_result<T>::type;
+template <class Fun, class... Args>
+using void_invoke_result_t = typename void_invoke_result<Fun, void, Args...>::type;
 
 template <class Fun, class... Args,
           class Result = void_invoke_result_t<Fun, Args...>>
 Result void_invoke(Fun&& fun, Args&&... args) {
     // TODO: maybe add macro to avoid repetition of std::invoke
-    if constexpr(std::is_void_v<Result>) {
+    if constexpr(std::is_same_v<Result, void_type>) {
         std::invoke(std::forward<Fun>(fun),
                     std::forward<Args>(args)...);
         return {};
