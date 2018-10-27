f4b51e4c51828d4c38a3bfac344e76ec48058502
Stefano Montesi <dincio.montesi@gmail.com>
2018-10-27 11:38:03 +0200

    Explored some properties of the MINDSUM problem operations.

---
+++ codechef/MINDSUM/python/main.py
@@ -0,0 +1,20 @@
+def digitsum(num):
+    if num == 0:
+        return 0
+    else:
+        return digitsum(num // 10) + num % 10
+
+j = None
+state = "init"
+
+for i in range(0, 100, int(input())):
+    s = digitsum(i)
+    if state == "init":
+        state = "const"
+        oldI = i
+        print("[{}".format(i), end="")
+    elif state == "const" and j is None or abs(s - j) != 1:
+        state = "init"
+        print("...{}]({})".format(i, i - oldI), end="\n")
+    j = s
+print()
