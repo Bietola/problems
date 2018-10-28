0a3ba07d2ffcf72b5c254a487dc56dba82489e13
Stefano Montesi <dincio.montesi@gmail.com>
2018-10-28 23:34:34 +0100

    Attempted MINDSUM recursive solution. Crash. Need to learn gdb.

--- codechef/MINDSUM/cpp/src/main.cpp
+++ codechef/MINDSUM/cpp/src/main.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
+#include <utility>
 
-using num = size_t;
+using namespace std;
 
-// operations
-num add(num lhs, num rhs) {
-    return lhs + rhs;
+using ll = long long;
+using pll = pair<ll, ll>;
+using uint = unsigned int;
+
+const uint MAX_DEPTH = 100;
+
+// outputs digitsum
+auto digitsum(ll num, ll sum = 0) {
+    if (num == 0)
+        return sum;
+    else
+        return digitsum(num / 10, sum + (num % 10));
 }
-num digitsum(num n) {
-    num s = 0;
-    for(s = 0; n > 0; n /= 10) {
-        s += (n % 10);
+
+// prints: minimum value, minimum num of ops
+pll solve(int n, int d, uint depth = 0) {
+    if (depth > MAX_DEPTH) {
+        return { n, depth };
     }
-    return s;
+    return min(solve(n + d, d), solve(digitsum(n), d),
+               [] (const auto& lhs, const auto& rhs) {
+                   return lhs.first < rhs.first &&
+                          lhs.second < rhs.second;
+               });
 }
 
-// solve
-pair<num, size_t> solve(num n, num d) {
-
-}
-
+// MAIN
 int main() {
-    std::cout << "digitsum(10): " << digitsum(10234) << "\n";
+    uint T;
+    cin >> T;
+
+    for(uint j = 0; j < T; ++j) {
+        ll n, d;
+        cin >> n >> d;
+        auto solution = solve(n, d);
+        cout << solution.first << " " << solution.second << "\n";
+    }
 
     return 0;
 }
--- codechef/MINDSUM/python/main.py
+++ codechef/MINDSUM/python/main.py
@@ -1,4 +1,4 @@
-def digitsum(num):
+ef digitsum(num):
     if num == 0:
         return 0
     else:
