5f900a4ab30fc3126fb6c3d61bfa05104cee07f8
Stefano Montesi <dincio.montesi@gmail.com>
2018-11-01 23:00:27 +0100

    Turned recursive approach into iterative. Still does not work... need to identify bottleneck.

--- codechef/MINDSUM/cpp/src/main.cpp
+++ codechef/MINDSUM/cpp/src/main.cpp
@@ -1,61 +1,50 @@
 #include <iostream>
-#include <utility>
 #include <queue>
-#include <array>
+#include <unordered_map>
 
 using namespace std;
 
 using ll = long long;
 using pll = pair<ll, ll>;
-using uint = unsigned int;
 
-const uint MAX_DEPTH = 50;
-
-// outputs digitsum
-auto digitsum(ll num, ll sum = 0) {
-    if (num == 0)
-        return sum;
-    else
-        return digitsum(num / 10, sum + (num % 10));
-}
-
-// prints: minimum value, minimum num of ops
-pll solve(ll n, ll d, ll target, ll depth,
-          queue<pll>& q, array<bool, 9>& visited) {
-    // visit node
-    if (n == target) {
-        return { target, depth };
-    }
-
-    // add children to queue
-    if (n >= 10) {
-        q.emplace(digitsum(n), depth + 1);
-        q.emplace(n + d, depth + 1);
-    } 
-    else if (!visited[n - 1]) {
-        visited[n - 1] = true;
-        q.emplace(n + d, depth + 1);
-    }
-
-    // visit next in queue
-    auto nextNode = q.front(); q.pop();
-    return solve(nextNode.first, d, target, nextNode.second, q, visited);
+ll digitsum(ll n, ll acc = 0) {
+    if (n == 0) return acc;
+    else return digitsum(n / 10, acc + n % 10);
 }
 
 pll solve(ll n, ll d) {
-    queue<pll> q = {};
-    array<bool, 9> visited = { false };
-    ll target = ((n % 3 == 0) && (d % 3 == 0) ? 3 : 1);
+    queue<pll> q;
+    unordered_map<ll, ll> m;
+    q.emplace(n, 0);
 
-    return solve(n, d, target, 0, q, visited);
+    ll target = ((n % 3 == 0 && d % 3 == 0) ? 3 : 1);
+
+    while (!q.empty()) {
+        ll n = q.front().first;
+        ll ops = q.front().second;
+        q.pop();
+
+        if (n < 10) {
+            if (n == target) {
+                return { n, ops };
+            }
+            else if (m.find(n) == m.end()) {
+                m[n] = ops;
+                q.emplace(n + d, ops + 1);
+            }
+        }
+        else {
+            q.emplace(digitsum(n), ops + 1);
+            q.emplace(n + d, ops + 1);
+        }
+    }
 }
 
-// MAIN
 int main() {
-    uint T;
+    short T;
     cin >> T;
 
-    for(uint j = 0; j < T; ++j) {
+    while (T-- > 0) {
         ll n, d;
         cin >> n >> d;
         auto solution = solve(n, d);
