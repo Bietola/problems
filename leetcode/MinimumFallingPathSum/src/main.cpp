aaceb3593bdf19c94a02f9c37468d4313237bfc4
Stefano Montesi <dincio.montesi@gmail.com>
2018-11-04 16:26:25 +0100

    Solved problem brutally. Need to optimize and change design to use indices instead of iterators (maybe).

--- leetcode/MinimumFallingPathSum/src/main.cpp
+++ leetcode/MinimumFallingPathSum/src/main.cpp
@@ -1,53 +1,66 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <algorithm>
 #include <cassert>
 
 using namespace std;
 
 using ll = long long;
 
-void solve(const vector<vector<ll>>& input,
-           vector<ll>& output,
-           vector<vector<ll>>::iterator vecItr,
-           vector<ll>::iterator itr,
-           ll acc) {
-    if (vecItr == end(input)) {
-        output.push_back(acc);
+void getFallingSums(const vector<vector<ll>>& input,
+                    vector<ll>& output,
+                    vector<vector<ll>>::const_iterator vecItr,
+                    vector<ll>::const_iterator itr,
+                    ll acc) {
+    auto newAcc = acc + *itr;
+
+    if (vecItr == prev(cend(input))) {
+        output.push_back(newAcc);
     }
     else {
-        // center
-        solve(input, output, next(vecItr), itr, acc + *itr);
-        // left
-        if (prev(itr) != rend(/*TODO*/))
-            solve(input, output, next(vecItr), prev(itr), acc + *itr);
-        // right
-        if (next(itr) != end(/*TODO*/))
-            solve(input, output, next(vecItr), next(itr), acc + *itr);
+        // construct next things
+        auto nextVecItr = next(vecItr);
+        auto nextItr = cbegin(*next(vecItr)) + distance(cbegin(*vecItr), itr);
+        // recurse center
+        getFallingSums(input, output, next(vecItr), nextItr, newAcc);
+        // recurse left
+        if (prev(nextItr) != prev(cbegin(*nextVecItr)))
+            getFallingSums(input, output, next(vecItr), prev(nextItr), newAcc);
+        // recurse right
+        if (next(nextItr) != cend(*nextVecItr))
+            getFallingSums(input, output, next(vecItr), next(nextItr), newAcc);
     }
 }
 
-void solve(const vector<vector<ll>>& input,
-           vector<ll>& output) {
-    return solve(input, output, begin(input), begin(input.front()), 0L);
+ll solve(const vector<vector<ll>>& input) {
+    // get all sums
+    vector<ll> sums;
+    const auto& toprow = *cbegin(input);
+    for (auto itr = cbegin(toprow); itr != cend(toprow); ++itr) {
+        getFallingSums(input, sums, cbegin(input), itr, 0ll);
+    }
+
+    /* // DB */
+    /* for (const auto& ele : sums) { */
+    /*     cout << ele << ", "; */
+    /* } */
+    /* cout << endl; */
+    /* // END DB */
+
+    // return minimum
+    return *min_element(cbegin(sums), cend(sums));
 }
 
 int main() {
     vector<vector<ll>> input = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
+        {63, 70, 79, 18},
+        {90, 16, -10, 32},
+        {100, 84, 59, -84},
+        {23, -3, 26, 69}
     };
 
-    vector<ll> output;
-    solve(input, output);
-
-    for (const auto& row : output) {
-        for (const auto& ele : row) {
-            cout << ele << ", ";
-        }
-        cout << "\n";
-    }
+    cout << "minimum falling sum: " << solve(input) << "\n";
 
     return 0;
 }
