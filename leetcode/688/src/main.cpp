b3a47a3fb44514978ba7c4f58bd24e28ff07862c
Stefano Montesi <dincio.montesi@gmail.com>
2018-11-06 17:44:28 +0100

    Solved problem.

--- leetcode/688/src/main.cpp
+++ leetcode/688/src/main.cpp
@@ -2,12 +2,21 @@
 #include <vector>
 #include <array>
 #include <cmath>
+#include <numeric>
 
 using namespace std;
 
+// primitive types
+using ll = long long;
 // vectors
+template <class T> using v = vector<T>;
+using vi = vector<int>;
+using vll = vector<long long>;
+using vd = vector<double>;
 template <class T> using vv = vector<vector<T>>;
-using vvi = vv<int>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<long long>>;
+using vvd = vector<vector<double>>;
 // iteration
 #define RNGF(_VAR, _START, _FINISH)  for (int _VAR = _START; _VAR < _FINISH; ++_VAR)
 #define FORJ(_START, _FINISH) RNGF(j, _START, _FINISH)
@@ -17,51 +26,39 @@ using vvi = vv<int>;
 double solve(int N, int K, int r, int c) {
     // utility
     static array<pair<int, int>, 8> moves = {
-        pair<int, int>{-1, -2},
-        pair<int, int>{-2, -1},
-        pair<int, int>{1, -2},
-        pair<int, int>{2, -1},
-        pair<int, int>{-2, 1},
-        pair<int, int>{-1, 2},
-        pair<int, int>{2, 1},
-        pair<int, int>{1, 2}
+        pair<double, double>{-1, -2},
+        pair<double, double>{-2, -1},
+        pair<double, double>{1, -2},
+        pair<double, double>{2, -1},
+        pair<double, double>{-2, 1},
+        pair<double, double>{-1, 2},
+        pair<double, double>{2, 1},
+        pair<double, double>{1, 2}
     };
     auto iob = [N] (int x, int y) {
         return x >= 0 && x < N && y >= 0 && y < N;
     };
 
     // chessboard
-    vvi board;
-    board.resize(N);
-    FORJ (0, N) board[j].resize(N);
-
-    // construct initial board
-    FORJ (0, N) {
-        FORI (0, N) {
-            if (j == 0 || i == 0 || j == N - 1 || i == N - 1) board[j][i] = 2;
-            else if ((j == 1 || j == N - 2) && (i == 1 || i == N - 2)) board[j][i] = 4;
-            else if (j == 1 || i == 1 || j == N - 2 || i == N - 2) board[j][i] = 6;
-            else board[j][i] = 8;
-        }
-    }
+    vvd board(N, vd(N, 1));
 
     // progressively update board (dynamic programming)
-    FORJ (0, K - 1) {
-        vvi newboard;
-        newboard.resize(N);
-        FORJ (0, N) newboard[j].resize(N);
+    FORJ (0, K) {
+        vvd newboard(N, vd(N, 0));
         RNGF (x, 0, N) {
             RNGF (y, 0, N) {
-                newboard[x][y] = 0;
-                for (const auto& mv : moves) {
-                    auto mx = mv.first; 
-                    auto my = mv.second; 
-                    auto nx = x + mx;
-                    auto ny = y + my;
-                    if (iob(nx, ny)) {
-                        newboard[x][y] += board[nx][ny];
+                newboard[x][y] = accumulate(begin(moves), end(moves), 0.0,
+                    [&] (double sum, const auto& mv) {
+                        auto mx = mv.first; 
+                        auto my = mv.second; 
+                        auto nx = x + mx;
+                        auto ny = y + my;
+                        if (iob(nx, ny))
+                            return sum + board[nx][ny];
+                        else
+                            return sum;
                     }
-                }
+                ) / 8;
                 cout << newboard[x][y] << "; ";
             }
             cout << endl;
@@ -70,7 +67,7 @@ double solve(int N, int K, int r, int c) {
     }
 
     // return answer
-    return double(board[r][c])/pow(8, K);
+    return board[r][c];
 }
 
 main() {
