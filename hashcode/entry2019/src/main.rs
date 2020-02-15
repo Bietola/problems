32bf7f5929a5daa0861fd755d397ca7142498d55
Stefano Montesi <dincio.montesi@gmail.com>
2020-02-15 18:44:37 +0100

    Handle vertical pictures in construction of input graph

--- hashcode/entry2019/src/main.rs
+++ hashcode/entry2019/src/main.rs
@@ -6,7 +6,9 @@ use petgraph::{
 use std::collections::HashSet;
 use std::fs;
 
-#[derive(Debug, Clone, Copy)]
+type Tags = HashSet<String>;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Ornt {
     V,
     H,
@@ -24,54 +26,99 @@ struct Pict {
     tags: HashSet<String>,
 }
 
-fn interest_rating(pict1: &Pict, pict2: &Pict) -> usize {
+#[derive(Clone, Debug)]
+enum Slide {
+    O1(Pict),
+    V2(Pict, Pict),
+}
+impl Default for Slide {
+    fn default() -> Self {
+        unimplemented!()
+    }
+}
+impl Slide {
+    fn tags(&self) -> Tags {
+        // TODO: cache union and return &Tags
+        match self {
+            Slide::O1(p) => p.tags.clone(),
+            Slide::V2(p1, p2) => p1.tags.union(&p2.tags).map(String::clone).collect(),
+        }
+    }
+}
+
+fn interest_rating(slide1: &Slide, slide2: &Slide) -> usize {
     use std::cmp::min;
 
     min(
-        pict1.tags.difference(&pict2.tags).count(),
+        slide1.tags().difference(&slide2.tags()).count(),
         min(
-            pict2.tags.difference(&pict1.tags).count(),
-            pict1.tags.intersection(&pict2.tags).count(),
+            slide2.tags().difference(&slide1.tags()).count(),
+            slide1.tags().intersection(&slide2.tags()).count(),
         ),
     )
 }
 
-fn parse_input(path: &'static str) -> UnGraph<Pict, usize> {
+fn add_node_and_complete<N, E, F>(graph: &mut UnGraph<N, E>, new_node: N, connect_fun: F)
+where
+    F: Fn(&N, &N) -> E,
+    N: Default + Clone,
+{
+    let new_idx = graph.add_node(new_node.clone());
+
+    let new_edges: Vec<_> = graph
+        .node_weights_mut()
+        .enumerate()
+        .filter_map(|(old_i_idx, old_node)| {
+            let old_idx = NodeIndex::new(old_i_idx);
+
+            if old_idx == new_idx {
+                None
+            } else {
+                Some((old_idx, new_idx, connect_fun(old_node, &new_node)))
+            }
+        })
+    .collect();
+
+    graph.extend_with_edges(new_edges);
+}
+
+fn parse_input(path: &'static str) -> UnGraph<Slide, usize> {
     let raw = fs::read_to_string(path).unwrap();
 
     let mut result = Graph::new_undirected();
+    let mut vpicts = Vec::<Pict>::new();
 
     raw.lines().skip(1).for_each(|line| {
         let words: Vec<_> = line.split_whitespace().collect();
 
+        let ornt = if words[0] == "H" { Ornt::H } else { Ornt::V };
+
         let new_node = Pict {
-            ornt: if words[0] == "H" { Ornt::H } else { Ornt::V },
+            ornt,
             tags: words.into_iter().skip(2).map(&str::to_owned).collect(),
         };
-        let new_idx = result.add_node(new_node.clone());
 
-        let new_edges: Vec<_> = result
-            .node_weights_mut()
-            .enumerate()
-            .filter_map(|(old_i_idx, old_node)| {
-                let old_idx = NodeIndex::new(old_i_idx);
+        if ornt == Ornt::V {
+            // Create new node for every vertical picture already parsed (combined with the new
+            // one).
+            for vpict in vpicts.iter() {
+                // Every node created this way is still connected to every other node (the graph is
+                // kept complete).
+                add_node_and_complete(&mut result, Slide::V2(new_node.clone(), vpict.clone()), interest_rating);
+            }
 
-                if old_idx == new_idx {
-                    None
-                } else {
-                    Some((old_idx, new_idx, interest_rating(old_node, &new_node)))
-                }
-            })
-            .collect();
-
-        result.extend_with_edges(new_edges);
+            // Remember vertical picture parsed to repeat the process.
+            vpicts.push(new_node);
+        } else {
+            add_node_and_complete(&mut result, Slide::O1(new_node), interest_rating);
+        }
     });
 
     result
 }
 
-solve(input: UnGraph<Pict, usize>) {
-
+fn solve(input: UnGraph<Pict, usize>) {
+    unimplemented!()
 }
 
 fn main() {
