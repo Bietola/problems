35aa42147118cb6a5312a3911baee1f80bcb8b0b
Bietola <dincio.montesi@gmail.com>
2023-01-24 17:17:53 +0100

    Clean

--- jobs/gdlauncher/rust-engineer/src/main.rs
+++ jobs/gdlauncher/rust-engineer/src/main.rs
@@ -3,7 +3,7 @@
 use std::fs;
 use std::collections::{HashSet, VecDeque};
 
-cat assets/challenge_input.txt | nl | grep -E '^\s+\b118\b' -B 100 | grep -E '\b1\b'
+// cat assets/challenge_input.txt | nl | grep -E '^\s+\b118\b' -B 100 | grep -E '\b1\b'
 
 fn main() {
     let inp = fs::read_to_string("assets/challenge_input.txt").unwrap();
@@ -13,28 +13,25 @@ fn main() {
         .split("\n")
         .map(|line| {
             // println!("{}", line);
-            let n: i32 = line.parse().unwrap();
-            n
+            line.parse::<i32>().unwrap()
         });
 
     let mut win = VecDeque::from(inp.next_chunk::<100>().unwrap());
-    let mut set: HashSet<_> = win.iter().map(|e| *e).collect();
+    let mut set: HashSet<i32> = win.iter().copied().collect();
 
     let ok = inp.find(|&sum| {
-        let crash = win.iter()
+        win.iter()
+            .copied()
             // .map(|&lh| { println!("lh: {}", lh); lh })
-            .find(|&lh| set.contains(&(sum - lh)));
-
-        if crash.is_some() {
-            set.remove(
-                &win.pop_front().unwrap()
-            );
-            win.push_back(sum);
-            set.insert(sum);
-            false
-        } else {
-            true
-        }
+            .find(|lh| set.contains(&(sum - lh)))
+            .map(|_| {
+                set.remove(
+                    &win.pop_front().unwrap()
+                );
+                win.push_back(sum);
+                set.insert(sum);
+            })
+            .is_none()
     });
 
     println!("{:#?}", ok);
